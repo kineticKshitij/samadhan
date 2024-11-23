25988e3b53ae6dbc42a38e2b61055b8c67620f03
kineticKshitij <143507517+kineticKshitij@users.noreply.github.com>
2024-11-23 11:17:07 +0530

    Update settings.py

--- Chatbot/Chatbot/settings.py
+++ Chatbot/Chatbot/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 #     "/var/www/static/",
